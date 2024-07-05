4bf79ac8c13c16a24ba2e6400024769bff8a5116
tmash <132013534+7m45h@users.noreply.github.com>
2024-07-05 14:29:09 +0530

    refactor 0.1

--- evil_manager.py
+++ evil_manager.py
@@ -11,52 +11,63 @@ ARGS = PARSER.parse_args()
 CON = sqlite3.connect("./evil.db")
 CUR = CON.cursor()
 
-def newDatabase():
+def createTable():
     CUR.execute("CREATE TABLE IF NOT EXISTS movies(imdb TEXT PRIMARY KEY, name TEXT, year INTEGER, hash TEXT NULL, poster BLOB NULL)")
     CON.commit()
     print("[!] done")
 
-def addNew():
+def addMovie():
     print("[?]")
     imdb = input("    imdb: ")
-    existing_row = CUR.execute("SELECT imdb, name, year FROM movies WHERE imdb=?", (imdb,)).fetchone()
+    existingRow = CUR.execute("SELECT imdb, name, year FROM movies WHERE imdb=?", (imdb,)).fetchone()
     if existing_row is None:
         name = input("    name: ")
         year = input("    year: ")
         hash = input("    hash: ").upper()
-        poster = input("    poster: ")
+        posterPath = input("    poster: ")
         print("[!] summary")
         print(f"    imdb: {imdb}")
         print(f"    name: {name}")
         print(f"    year: {year}")
         print(f"    hash: {hash}")
-        print(f"    imge: {poster}")
+        print(f"    imge: {posterPath}")
 
-        with open(poster, "rb") as image:
-            poster_bytes = image.read();
+        posterBytes = None
 
-        write = input("[?] write to db (y/n): ")
-        if write == "y":
-            CUR.execute("INSERT INTO movies VALUES (?, ?, ?, ?, ?)", (imdb, name, year, hash, poster_bytes))
-            CON.commit()
+        try:
+            with open(posterPath, "rb") as posterFile:
+                posterBytes = posterFile.read()
+        except FileNotFoundError:
+            print(f"[!] poster path: {poster_path} does not exist")
+        except Exception as e:
+            print(f"[!] error: {e}")
+
+        if posterBytes is None:
+            print("[!] read poster failed")
         else:
-            print("\n[!] did not wrote to db")
+            write = input("[?] write to db (y/N): ")
+            if write == "y":
+                CUR.execute("INSERT INTO movies VALUES (?, ?, ?, ?, ?)", (imdb, name, year, hash, posterBytes))
+                CON.commit()
+            else:
+                print("\n[!] did not wrote to db")
+
     else:
         print("\n[!] allready exists")
-        print(f"\n    imdb: {existing_row[0]}")
-        print(f"\n    name: {existing_row[1]}")
-        print(f"\n    year: {existing_row[2]}")
+        print(f"\n    imdb: {existingRow[0]}")
+        print(f"\n    name: {existingRow[1]}")
+        print(f"\n    year: {existingRow[2]}")
 
-def listAll():
+def listAllMovies():
     for row in CUR.execute("SELECT rowid, imdb, name, year, hash FROM movies").fetchall():
         print(f"[{row[0]:04d}]  {row[1]: <10s}  {row[2]:.<55s}  {row[3]:4d}  {row[4]}")
 
-def editRow():
+def editMovie():
     for row in CUR.execute("SELECT rowid, imdb, name FROM movies").fetchall():
         print(f"[{row[0]:04d}] {row[1]} {row[2]}")
 
-    rowid = input("\n[?] rowid: ")
-    row = CUR.execute("SELECT imdb, name, year, hash, poster FROM movies WHERE rowid=?", (rowid,)).fetchone()
+    rowID = input("\n[?] rowid: ")
+    row = CUR.execute("SELECT imdb, name, year, hash, poster FROM movies WHERE rowid=?", (rowID,)).fetchone()
 
     if row is None:
         print("[!] id not found")
@@ -65,17 +76,18 @@ def editRow():
         name = row[1] if not (name := input("    name: ")) else name
         year = row[2] if not (year := input("    year: ")) else year
         hash = row[3] if not (hash := input("    hash: ")) else hash.upper()
-        poster_path = input("    poster: ")
+        posterPath = input("    poster: ")
 
-        if not poster_path:
-            poster_bytes = row[4]
+        posterBytes = None
+
+        if not posterPath:
+            posterBytes = row[4]
         else:
             try:
-                image_file = open(poster_path, "rb")
-                poster_bytes = image_file.read()
-                image_file.close()
+                with open(posterPath, "rb") as posterFile:
+                    posterBytes = posterFile.read()
             except FileNotFoundError:
-                print(f"[!] poster path: {poster_path} does not exist")
+                print(f"[!] poster path: {posterPath} does not exist")
             except Exception as e:
                 print(f"[!] error: {e}")
 
@@ -85,20 +97,20 @@ def editRow():
         print(f"    year: {row[2]} -> {year}")
         print(f"    hash: {row[3]} -> {hash}")
 
-        edit = input("\n[?] edit (y/n): ")
+        edit = input("\n[?] edit (y/N): ")
         if edit == 'y':
-            CUR.execute("UPDATE movies SET imdb=?, name=?, year=?, hash=?, poster=? WHERE rowid=?", (imdb, name, year, hash, poster_bytes, rowid))
+            CUR.execute("UPDATE movies SET imdb=?, name=?, year=?, hash=?, poster=? WHERE rowid=?", (imdb, name, year, hash, posterBytes, rowID))
             CON.commit()
             print("[!] done")
         else:
             print("[!] did not edited")
 
-def deleteRow():
+def deleteMovie():
     imdb = input("[?] imdb: ")
     row = CUR.execute("SELECT rowid, name, year FROM movies WHERE imdb=?", (imdb,)).fetchone()
     print("[!]")
     print(f"[{row[0]}]\t{row[1]}\t{row[2]}")
-    delete = input("[?] delete (y/n): ")
+    delete = input("[?] delete (y/N): ")
 
     if delete == "y":
         CUR.execute("DELETE FROM movies WHERE imdb=?", (imdb,))
@@ -107,33 +119,39 @@ def deleteRow():
     else:
         print("[!] did not deleted")
 
-def tomlOutput():
-    with open("movies.toml", "w") as tFile:
-        for row in CUR.execute("SELECT imdb, name, year, hash, poster FROM movies ORDER BY name").fetchall():
-            imdb = row[0]
-            name = row[1]
-            year = row[2]
-            hash = row[3]
-            poster_bytes = row[4]
+def exportDB():
+    try:
+        with open("movies.toml", "w") as tomlFile:
+            for row in CUR.execute("SELECT imdb, name, year, hash, poster FROM movies ORDER BY name").fetchall():
+                imdb = row[0]
+                name = row[1]
+                year = row[2]
+                hash = row[3]
+                posterBytes = row[4]
 
-            tFile.write(f"""[[movies]]\nimdb = "{imdb}"\nname = "{name}"\nyear = "{year}"\n""")
-            with open(f"./outputs/{imdb}.jpg", "wb") as pFile:
-                pFile.write(poster_bytes)
+                tomlFile.write(f"""[[movies]]\nimdb = "{imdb}"\nname = "{name}"\nyear = "{year}"\n""")
+                with open(f"./outputs/{imdb}.jpg", "wb") as posterFile:
+                    posterFile.write(posterBytes)
+    except Exception as e:
+        print(f"[!] error: {e}")
 
-if ARGS.mode == 'n':
-    newDatabase()
-elif ARGS.mode == 'a':
-    addNew()
-elif ARGS.mode == 'l':
-    listAll()
-elif ARGS.mode == 'e':
-    editRow()
-elif ARGS.mode == 'd':
-    deleteRow()
-elif ARGS.mode == 'o':
-    tomlOutput()
-else:
-    print("[!] error")
+MODES = {
+    'n': createTable,
+    'a': addMovie,
+    'l': listAllMovies,
+    'e': editMovie,
+    'd': deleteMovie,
+    'o': exportDB
+}
+
+try:
+    action = MODES[ARGS.mode]
+except KeyError:
+    print("[!] undefined mode")
+except Exception as e:
+    print(f"[!] error: {e}")
+
+action()
 
 CON.close()
 exit()
