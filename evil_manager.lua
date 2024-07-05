d6969d5c3bb8b9a940fd8053860002e8ce8d922d
tmash <132013534+7m45h@users.noreply.github.com>
2024-07-05 13:18:18 +0530

    base test

--- evil_manager.lua
+++ evil_manager.lua
@@ -1,3 +1,38 @@
 #! /usr/bin/env lua
 
-print 'Hello, Moon!'
+local argparse = require 'argparse'
+local sqlite   = require 'lsqlite3'
+
+local parser = argparse()
+  :name 'evil_manager'
+  :description 'manage evil databse'
+  :epilog 'find more @ https://github.com/7m45h/evil_manager'
+
+parser:argument()
+  :name 'mode'
+  :description 'n: new table a: add l: list all e: edit d: delete o: toml output'
+  :choices { 'n', 'a', 'l', 'e', 'd', 'o' }
+
+local args = parser:parse()
+
+local db = sqlite.open('./evil.db')
+
+local function listMovies()
+  for rowid, imdb, name, year, hash in db:urows 'SELECT rowid, imdb, name, year, hash FROM movies' do
+    print(string.format('[%04d]  %-10s  %-55s  %4d  %s', rowid, imdb, name, year, hash))
+  end
+end
+
+local modes = {
+  l = listMovies
+}
+
+local action = modes[args.mode]
+
+if action then
+  action()
+else
+  print '[!] undefined mode'
+end
+
+db:close()
