da968d82058c115d59274abdf76365a13821afaf
tmash <132013534+7m45h@users.noreply.github.com>
2024-07-05 16:49:58 +0530

    add movie

--- evil_manager.lua
+++ evil_manager.lua
@@ -11,7 +11,7 @@ local parser = argparse()
 parser:argument()
   :name 'mode'
   :description 'n: new table a: add l: list all e: edit d: delete o: toml output'
-  :choices { 'n', 'l' }
+  :choices { 'n', 'a', 'l' }
 
 local args = parser:parse()
 
@@ -19,6 +19,39 @@ local db = sqlite.open('./evil.db')
 
 local function create_table()
   db:execute('CREATE TABLE IF NOT EXISTS movies(imdb TEXT PRIMARY KEY, name TEXT, year INTEGER, hash TEXT NULL, poster BLOB NULL)')
+end
+
+local function add_movie()
+  print('[?]')
+  io.write('    imdb: ')
+  local imdb = io.read()
+  local sql_statement = db:prepare('SELECT imdb, name, year FROM movies WHERE imdb=?')
+  sql_statement:bind(1, imdb)
+  local stmt_status = sql_statement:step()
+  if stmt_status == sqlite.ROW then
+    local existing_row = sql_statement:get_named_values()
+    print("\n[!] allready exists")
+    print(string.format("    imdb: %s", existing_row.imdb))
+    print(string.format("    name: %s", existing_row.name))
+    print(string.format("    year: %d", existing_row.year))
+
+  elseif stmt_status == sqlite.DONE then
+    io.write('    name: ')
+    local name = io.read()
+    io.write('    year: ')
+    local year = io.read()
+    io.write('    hash: ')
+    local hash = string.upper(io.read())
+    io.write('    poster_path: ')
+    local poster_path = io.read()
+
+    print("\n[!] summary")
+    print(string.format("    imdb: %s", imdb))
+    print(string.format("    name: %s", name))
+    print(string.format("    year: %d", year))
+    print(string.format("    hash: %s", hash))
+    print(string.format("    imge: %s", poster_path))
+  end
 end
 
 local function list_movies()
@@ -29,6 +62,7 @@ end
 
 local modes = {
   n = create_table,
+  a = add_movie,
   l = list_movies
 }
 
