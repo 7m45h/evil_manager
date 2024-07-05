c2193c0df9aa45da8329be3e3d962e9f5d428320
tmash <132013534+7m45h@users.noreply.github.com>
2024-07-05 16:48:55 +0530

    create table

--- evil_manager.lua
+++ evil_manager.lua
@@ -1,7 +1,7 @@
 #! /usr/bin/env lua
 
-local argparse = require 'argparse'
-local sqlite   = require 'lsqlite3'
+local argparse = require('argparse')
+local sqlite   = require('lsqlite3')
 
 local parser = argparse()
   :name 'evil_manager'
@@ -11,20 +11,25 @@ local parser = argparse()
 parser:argument()
   :name 'mode'
   :description 'n: new table a: add l: list all e: edit d: delete o: toml output'
-  :choices { 'n', 'a', 'l', 'e', 'd', 'o' }
+  :choices { 'n', 'l' }
 
 local args = parser:parse()
 
 local db = sqlite.open('./evil.db')
 
-local function listMovies()
-  for rowid, imdb, name, year, hash in db:urows 'SELECT rowid, imdb, name, year, hash FROM movies' do
+local function create_table()
+  db:execute('CREATE TABLE IF NOT EXISTS movies(imdb TEXT PRIMARY KEY, name TEXT, year INTEGER, hash TEXT NULL, poster BLOB NULL)')
+end
+
+local function list_movies()
+  for rowid, imdb, name, year, hash in db:urows('SELECT rowid, imdb, name, year, hash FROM movies') do
     print(string.format('[%04d]  %-10s  %-55s  %4d  %s', rowid, imdb, name, year, hash))
   end
 end
 
 local modes = {
-  l = listMovies
+  n = create_table,
+  l = list_movies
 }
 
 local action = modes[args.mode]
@@ -32,7 +37,7 @@ local action = modes[args.mode]
 if action then
   action()
 else
-  print '[!] undefined mode'
+  print('[!] undefined mode')
 end
 
 db:close()
